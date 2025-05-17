b558e11c2493392c1f3ec4418d3b6bb811af9d1c
Ekt Gupta <your-email@example.com>
2025-05-18 01:40:23 +0530

    Fix: define port variable for deployment

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pickle
 import requests
 from flask import Flask, render_template, request
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -193,6 +194,6 @@ def home():
     return render_template('index.html', movie_list=movies['title'].values)
 
 
-import os
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=True)  
